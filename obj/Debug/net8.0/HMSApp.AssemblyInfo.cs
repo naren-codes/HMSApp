e6409835d44daf03c1de54c1f29d5abe500b3eda
Sridhar <2431425@cognizant.com>
2025-08-13 12:05:04 +0530

    contact

--- obj/Debug/net8.0/HMSApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HMSApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMSApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbb21a6c3e6a50488d47c1e805ed40e025e196c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476fbbe5fb236910fe5fd6bc84fe0d8403e2785e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMSApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMSApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
